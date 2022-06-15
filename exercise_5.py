0da4c823f1f45d8d3d2a54fd1c74895140ca1388
Carlos Pavajeau <cantte098@gmail.com>
2022-06-14 19:10:26 -0500

    feat: add calculate final notes

--- exercise_5.py
+++ exercise_5.py
@@ -12,3 +12,16 @@ def load_notes(filename: str) -> list[list[float]]:
             notes.append(row)
 
     return list(map(lambda x: list(map(float, x)), notes))
+
+
+def calculate_final_notes(notes: list[list[float]]) -> list[float]:
+    """
+    Calculates the final notes of a list of notes. The final note is calculated by:
+    final_note = (note1 + note2) * 0.3 + note3 * 0.6
+    """
+    final_notes = []
+    for note in notes:
+        final_note = (note[0] + note[1]) * 0.3 + note[2] * 0.6
+        final_notes.append(round(final_note, 1))
+
+    return final_notes
--- exercise_5_spec.py
+++ exercise_5_spec.py
@@ -1,11 +1,14 @@
 import unittest
 
-from exercise_5 import load_notes
+from exercise_5 import load_notes, calculate_final_notes
 
 
 class Exercise5Spec(unittest.TestCase):
     def test_load_notes(self):
         self.assertEqual(load_notes('notes.csv'), [[1, 2, 3], [4, 5, 3]])
+
+    def test_calculate_final_notes(self):
+        self.assertEqual(calculate_final_notes([[4, 3, 3], [4, 5, 3]]), [3.9, 4.5])
 
 
 if __name__ == '__main__':
